60895c992535274b8e7f855b605ff443c77eac76
Jim Argeropoulos <tenholeharp@gmail.com>
2013-04-06 14:04:13 -0400

    Added a constructable HttpActionDescriptor object

---
+++ Peiro.Tests/TestableActionDescriptorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Http.Controllers;
+using Xunit;
+using System.Threading;
+
+namespace Peiro.Tests
+{
+	public class TestableActionDescriptorTests
+	{
+		[Fact]
+		public void DefaultConstructor_CastableToActionDescriptor()
+		{
+			var sut = new TestableActionDescriptor();
+
+			Assert.NotNull(sut as HttpActionDescriptor);
+		}
+		[Fact]
+		public void ActionName_ReturnsNull()
+		{
+			var sut = new TestableActionDescriptor();
+
+			Assert.Null(sut.ActionName);
+		}
+		[Fact]
+		public void ExecuteAsync_ReturnsNull()
+		{
+			var sut = new TestableActionDescriptor();
+
+			Assert.Null(sut.ExecuteAsync(null, null, CancellationToken.None));
+		}
+		[Fact]
+		public void GetParameters_ReturnsNull()
+		{
+			var sut = new TestableActionDescriptor();
+
+			Assert.Null(sut.GetParameters());
+		}
+		[Fact]
+		public void ReturnType_ReturnsNull()
+		{
+			var sut = new TestableActionDescriptor();
+
+			Assert.Null(sut.ReturnType);
+		}
+	}
+}
---
+++ Peiro/TestableActionDescriptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http.Controllers;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Peiro
+{
+	public class TestableActionDescriptor : HttpActionDescriptor
+	{
+		public override string ActionName
+		{
+			get { return null; }
+		}
+		public override Task<object> ExecuteAsync(HttpControllerContext controllerContext, IDictionary<string, object> arguments, CancellationToken cancellationToken)
+		{
+			return null;
+		}
+		public override Collection<HttpParameterDescriptor> GetParameters()
+		{
+			return null;
+		}
+		public override Type ReturnType
+		{
+			get { return null; }
+		}
+	}
+}
