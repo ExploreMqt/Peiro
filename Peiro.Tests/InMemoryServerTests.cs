4cb0b7905dbc5d64623bc41810e5e7403e0eab73
Jim Argeropoulos <tenholeharp@gmail.com>
2013-04-07 00:55:27 -0400

    Added an HttpServer factory to enable integration testing.

---
+++ Peiro.Tests/InMemoryServerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Xunit;
+
+namespace Peiro.Tests
+{
+	public class InMemoryServerTests
+	{
+		[Fact]
+		public void Create_RigistrationAction_ActionCalled()
+		{
+			bool called = false;
+			
+			InMemoryServer.Create(config => called = true);
+
+			Assert.True(called);
+		}
+		[Fact]
+		public void Create_RigistrationAction_RecievesNonNullConfiguration()
+		{
+			InMemoryServer.Create(config => Assert.NotNull(config));
+		}
+		[Fact]
+		public void Create_NoOpRegistrationAction_IncludeErrorDetailPolicySetToAlways()
+		{
+			var server = InMemoryServer.Create(a => { });
+
+			Assert.Equal(IncludeErrorDetailPolicy.Always, server.Configuration.IncludeErrorDetailPolicy);
+			server.Dispose();
+		}
+		[Fact]
+		public void Create_NoOpRegistryAction_MessageHanderContainsPassThroughMessageHandler()
+		{
+			var server = InMemoryServer.Create(a => { });
+			
+			Assert.NotNull(server.Configuration.MessageHandlers.Where(h => h is PassThroughMessageHandler).FirstOrDefault());
+			server.Dispose();
+		}
+		[Fact]
+		public void Create_ConfigUsedInRegistration_IsInReturnedServer()
+		{
+			HttpConfiguration appliedToRegistration = null;
+
+			var server = InMemoryServer.Create(c => appliedToRegistration = c);
+
+			Assert.Same(appliedToRegistration, server.Configuration);
+			server.Dispose();
+		}
+	}
+}
---
+++ Peiro/InMemoryServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Peiro
+{
+	/// <summary>
+	/// Creates an instance of HttpServer and gives it the PassThroughMessageHandler, allowing nearly the 
+	/// full stack to be in play for testing. It's primary purpose is to enable integration testing.
+	/// </summary>
+	public class InMemoryServer
+	{
+		public static HttpServer Create(Action<HttpConfiguration> registrationAction)
+		{
+			var config = new HttpConfiguration();
+			registrationAction(config);
+			config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+			config.MessageHandlers.Add(new PassThroughMessageHandler());
+			return new HttpServer(config);
+		}
+	}
+}
