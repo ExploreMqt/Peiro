c4d2412c2ed895d3eeeaf967caf069f799296f0a
Jim Argeropoulos <tenholeharp@gmail.com>
2013-04-07 14:53:22 -0400

    Added a simple IDependencyResolver for those who choose not to use an IoC and need to inject a type at test time.

---
+++ Peiro.Tests/FakeResolverTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Http.Dependencies;
+using Xunit;
+
+namespace Peiro.Tests
+{
+	public class FakeResolverTests
+	{
+		[Fact]
+		public void Constructor_ImplementsIDependencyResolver()
+		{
+			Assert.IsAssignableFrom<IDependencyResolver>(new FakeResolver());
+		}
+		internal class Widget : IDisposable
+		{
+			public void Dispose(){}
+		}
+		[Fact]
+		public void Register_Widget_TupleOfDesiredTypeAndFactory()
+		{
+			object typeAndFactory = FakeResolver.Register(typeof(Widget), () => new Widget());
+			
+			Assert.IsType<Tuple<Type,Func<object>>>(typeAndFactory);
+		}
+		[Fact]
+		public void GetService_NonRegistered_Null()
+		{
+			var sut = new FakeResolver();
+
+			var instance = sut.GetService(typeof(Widget));
+
+			Assert.Null(instance);
+		}
+		[Fact]
+		public void GetService_WidgetRegistered_InstanceOfWidget()
+		{
+			var sut = new FakeResolver(FakeResolver.Register(typeof(Widget), () => new Widget()));
+
+			var result = sut.GetService(typeof(Widget));
+
+			Assert.IsType<Widget>(result);
+		}
+	}
+}
---
+++ Peiro/FakeResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+
+namespace Peiro
+{
+	/// <summary>
+	/// This enables you to supply fake implementations to sections of code that doesn't have a direct public interface.
+	/// NOTE: If you are using an IoC, you will not need this class.
+	/// </summary>
+	public class FakeResolver : IDependencyResolver
+	{
+		public static Tuple<Type, Func<object>> Register(Type type, Func<object> factory)
+		{
+			return new Tuple<Type,Func<object>>(type, factory);
+		}
+		private readonly Tuple<Type, Func<object>>[] registered;
+		public FakeResolver(params Tuple<Type, Func<object>>[] resolvers)
+		{
+			registered = resolvers; 
+		}
+		public IDependencyScope BeginScope()
+		{
+			throw new NotImplementedException();
+		}
+
+		public object GetService(Type serviceType)
+		{
+			var resolver = Array.Find(registered, r => r.Item1 == serviceType);
+			if (resolver != null)
+				return resolver.Item2();
+			return null;
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return new List<object>();
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
